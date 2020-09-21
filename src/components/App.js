6c92a4601d2a76878cb233654d2544d378c92ea4
youkey1001 <youkey_1001@yahoo.co.jp>
2020-09-21 21:23:31 +0900

    [add]complete lesson-3

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,14 @@ import React from 'react';
 class App extends React.Component {
     render() {
         return (
-            <div>
-                {/* <h1>タグを用いて、「Hello World」と表示してください */}
-                <h1>Hello World</h1>
-                
-                {/* <p>タグを用いて、「一緒にReactを学びましょう！」と表示してください */}
-                <p>一緒にReactを学びましょう！</p>
-
-                {/* <img>タグを用いて、画像を表示してください */}
-                <img src="./src/img/ninjawanko.png"/>
-                
-            </div>
+        <div>
+            <h1>こんにちは、にんじゃわんこさん！</h1>
+            {/* <button>タグ「ひつじ仙人」を追加してください */}
+            <button>ひつじ仙人</button>
+            
+            {/* <button>タグ「にんじゃわんこ」を追加してください */}
+            <button>にんじゃわんこ</button>
+    	</div>
         );
     }
 }
